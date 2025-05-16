a3c9e737f66cc19bdffec4c9a63a05e36556297d
Your Name <you@example.com>
2025-05-16 10:43:57 -0400

    Inicio proyecto

---
+++ microservicio-orden/microservicio-orden/src/main/java/com/perfulandia/cl/microservicio_orden/MicroservicioOrdenApplication.java
@@ -0,0 +1,13 @@
+package com.perfulandia.cl.microservicio_orden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioOrdenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioOrdenApplication.class, args);
+	}
+
+}
