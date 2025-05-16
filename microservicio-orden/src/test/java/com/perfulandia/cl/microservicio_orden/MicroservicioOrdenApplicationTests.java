6220fd553388f33a1e3f2337f49801e168329f93
Your Name <you@example.com>
2025-05-16 10:48:25 -0400

    inicio preoyecto

---
+++ microservicio-orden/src/test/java/com/perfulandia/cl/microservicio_orden/MicroservicioOrdenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfulandia.cl.microservicio_orden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioOrdenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
